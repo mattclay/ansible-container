1a1d976000219832bdfb77b07c51627ea28da6f2
Matt Clay <matt@mystile.com>
2016-06-20 12:38:29 -0700

    Fix shippable issues.

--- tests/utils/shippable.sh
+++ tests/utils/shippable.sh
@@ -9,15 +9,22 @@ pip install -r requirements.txt -r tests/requirements.txt
 python setup.py develop
 docker version
 docker-compose version
+
+function finish
+{
+    # make sure shippable has testresults and codecoverage even on build failure
+    
+    mkdir -p shippable/testresults
+    mkdir -p shippable/codecoverage
+
+    for type in unit integration; do
+        cp -av tests/reports/${type}/junit.xml    shippable/testresults/${type}.xml  || true
+        cp -av tests/reports/${type}/coverage.xml shippable/codecoverage/${type}.xml || true
+    done
+}
+
+trap finish EXIT INT TERM
 tests/utils/run_tests.sh
-
-mkdir -p shippable/testresults
-mkdir -p shippable/codecoverage
-
-for type in unit integration; do
-    cp -av tests/reports/${type}/junit.xml    shippable/testresults/${type}.xml
-    cp -av tests/reports/${type}/coverage.xml shippable/codecoverage/${type}.xml
-done
 
 bash <(curl -s https://codecov.io/bash) -F unit        -f tests/reports/unit/coverage.xml
 bash <(curl -s https://codecov.io/bash) -F integration -f tests/reports/integration/coverage.xml
